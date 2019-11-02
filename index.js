ae37c337ecc0fd5aff2f523453f95aa5da650a5f
Aykuli <aykuli@ya.ru>
2019-11-02 14:51:39 +0700

    feat: add lang changing

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ keyboardKeys = [
     ["", "Digit0", "0", ")", "0", ")"],
     ["", "Minus", "-", "_", "-", "_"],
     ["", "Equal", "=", "+", "=", "+"],
-    ["backspace", "Backspace", "Backspace", "", "", ""]
+    ["backspace", "Backspace", "Backspace", "Backspace", "Backspace", "Backspace"]
   ],
   [
-    ["tab", 'Tab', 'Tab', '', '', ''],
+    ["tab", 'Tab', 'Tab', 'Tab', 'Tab', 'Tab'],
     ["", 'KeyQ', "й", "Й", "q", "Q"],
     ["", 'KeyW', "ц", "Ц", "w", "W"],
     ['', 'KeyE', "у", "У", "e", "E"],
@@ -30,10 +30,10 @@ keyboardKeys = [
     ['', 'BracketLeft', "х", "Х", "[", "{"],
     ['', 'BracketRight', "ъ", "Ъ", "]", "}"],
     ["", 'Backslash', "\\", "/", "\\", "|"],
-    ['del', "Delete", 'Del', '', '', '', '']
+    ['del', "Delete", 'Del', 'Del', 'Del', 'Del', 'Del']
   ],
   [
-    ["capslock", 'CapsLock', 'CapsLock', '', '', '', ''],
+    ["capslock", 'CapsLock', 'CapsLock', 'CapsLock', 'CapsLock', 'CapsLock', 'CapsLock'],
     ['', 'KeyA', "ф", "Ф", "a", "A"],
     ['', 'KeyS', "ы", "Ы", "s", "S"],
     ['', 'KeyD', "в", "В", "d", "D"],
@@ -45,10 +45,10 @@ keyboardKeys = [
     ['', "KeyL", "д", "Д", "l", "L"],
     ['', 'Semicolon', "ж", "Ж", ";", ":"],
     ["", "Quote", "э", "Э", "'", '"'],
-    ['enter', "Enter", 'Enter', '', '' ,'']
+    ['enter', "Enter", 'Enter', 'Enter', 'Enter' ,'Enter']
   ],
   [
-    ['shift', "Shift", 'Shift', '', '', ''],
+    ['shift', "Shift", 'Shift', 'Shift', 'Shift', 'Shift'],
     ['', "KeyZ", "я", "Я", "z", "Z"],
     ['', "KeyX", "ч", "Ч", "x", "X"],
     ['', "KeyC", "с", "С", "c", "C"],
@@ -59,23 +59,23 @@ keyboardKeys = [
     ['', "Comma", "б", "Б", ".", "<"],
     ['', "Period", "ю", "Ю", ",", ">"],
     ['', 'Slash', ".", ",", "/", "?"],
-    ['', 'ArrowUp', "▲", "", "", ""],
-    ['shift', "Shift", 'Shift', '', '', '']
+    ['', 'ArrowUp', "▲", "▲", "▲", "▲"],
+    ['shift', "Shift", 'Shift', 'Shift', 'Shift', 'Shift']
   ],
   [
-    ["ctrl", "Ctrl", 'Ctrl', '', '', ''],
-    ["win", 'Win', 'Win', '', '', ''],
-    ["alt", "Alt", "Alt", '', '', ''],
+    ["ctrl", "Ctrl", 'Ctrl', 'Ctrl', 'Ctrl', 'Ctrl'],
+    ["win", 'Win', 'Win', 'Win', 'Win', 'Win'],
+    ["alt", "Alt", "Alt", 'Alt', 'Alt', 'Alt'],
     ["space", "Space", " ", " ", '', ''],
-    ["alt", "Alt", "Alt", '', '', ''],
+    ["alt", "Alt", "Alt", 'Alt', 'Alt', 'Alt'],
     ["", 'ArrowLeft', "<", "<", "<", "<"],
     ['', "ArrowDown", "▼", "▼", "▼", "▼"],
     ['', 'ArrowRight', ">", ">", ">", ">"],
-    ["ctrl", "Ctrl", 'Ctrl', '', '', '']
+    ["ctrl", "Ctrl", 'Ctrl', 'Ctrl', 'Ctrl', 'Ctrl']
   ]
 ];
 
-// Creating keyboard elements
+// Creating textaree and keyboard div container
 let keys = [["Backquote", "Digit1", "Minus", "Equal", "Backspace"]];
 let form = document.createElement("form");
 form.className = "wrapper";
@@ -90,19 +90,84 @@ let keyboard = document.createElement("div");
 keyboard.className = "keyboard";
 form.append(keyboard);
 
+//KeyboardLanguage changing
+let lang = localStorage.getItem('virtualKeyboardLang');
+if ( lang === null) {
+  localStorage.setItem('virtualKeyboardLang', 'en');
+}
+document.addEventListener('keydown', function(evt) {
+  if (evt.shiftKey && evt.altKey) {
+    (lan === 'en') ? localStorage.setItem('virtualKeyboardLang', 'ru') : localStorage.setItem('virtualKeyboardLang', 'en');
+    location.reload();
+  }  
+});
+
 let rowNumbers = [14, 15, 13, 13, 9];
 for (let i = 0; i < 6; i++) {
   var row = document.createElement("div");
   row.className = "row";
   keyboard.append(row);
-    for (let j = 0; j < rowNumbers[i]; j++) {
-      let key = document.createElement("button");
-      key.className = "key " + keyboardKeys[i][j][0];
-      row.append(key);
 
-      let span = document.createElement("span");
-      span.className = keyboardKeys[i][j][1];
-      key.append(span);
-      span.insertAdjacentText("afterbegin", keyboardKeys[i][j][2]);
+  for (let j = 0; j < rowNumbers[i]; j++) {
+    let key = document.createElement("button");
+    key.className = "key " + keyboardKeys[i][j][0];
+    row.append(key);
+
+    let spanEn = document.createElement("span");
+    let spanEnUp = document.createElement("span");
+    let spanEnDown = document.createElement("span");
+    let spanRu = document.createElement("span");
+    let spanRuUp = document.createElement("span");
+    let spanRuDown = document.createElement("span");
+
+    let [langOn, langOff] = [' on', ' off'];
+    if (lang == 'en') {
+      langOn = ' on';
+      langOff=  ' off';
+    } else {
+      langOn = ' off';
+      langOff=  ' on';
     }
+    spanEn.className = keyboardKeys[i][j][1] + langOn;
+    spanRu.className = keyboardKeys[i][j][1] + langOff;
+
+    key.append(spanEn);
+    key.append(spanRu);
+
+    spanRuDown.className = 'case down';
+    spanRu.append(spanRuDown);
+    spanRuDown.insertAdjacentText("afterbegin", keyboardKeys[i][j][2]);
+
+    spanRuUp.className = 'case up';
+    spanRu.append(spanRuUp);
+    spanRuUp.insertAdjacentText("afterbegin", keyboardKeys[i][j][3]);
+
+
+    spanEnDown.className = 'case down';
+    spanEn.append(spanEnDown);
+    spanEnDown.insertAdjacentText("afterbegin", keyboardKeys[i][j][4]);
+
+    spanEnUp.className = 'case up';
+    spanEn.append(spanEnUp);
+    spanEnUp.insertAdjacentText("afterbegin", keyboardKeys[i][j][5]);
+  }
 }
+
+keyboard.addEventListener('click', function(evt) {
+  let targetBtn = evt.target.closest('button');
+  console.dir(targetBtn);
+  
+  targetLang = targetBtn.querySelector('.on');
+  console.log(targetLang.className.split(' ')[0]);
+
+  keyboardKeys.forEach(row => {
+    row.forEach(el => {
+      if (el == targetLang.className.split(' ')[0]) {
+        
+        console.dir('el =', el);
+      }
+    });
+  });
+});
+
+
